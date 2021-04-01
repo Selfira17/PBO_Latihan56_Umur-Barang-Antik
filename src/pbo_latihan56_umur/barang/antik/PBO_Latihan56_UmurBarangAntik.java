036abd2f4bc75c6564a8f6d621d20a4197f34ec2
selfira <selfiraayw@gmail.com>
2021-04-01 20:42:01 +0700

    PBO latihan56 Umur Barang antik

---
+++ src/pbo_latihan56_umur/barang/antik/PBO_Latihan56_UmurBarangAntik.java
@@ -0,0 +1,29 @@
+/*
+ * Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+Deskripsi : Latihan 56-Umur Barang Antik
+ */
+package pbo_latihan56_umur.barang.antik;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO_Latihan56_UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    String nama = "Radio AM";
+    String Umur = "234 Tahun";
+    
+    System.out.println("Nama Barang Antik  :"+nama);
+    System.out.println("Umur Barang Antik ini adalah  :"+Umur);
+    
+    
+    }
+    
+}
